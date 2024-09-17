2482763ad6e0d3b9f2711eba4ef143405a2816b7
Cray0la <tyler.chappos@gmail.com>
2024-09-17 15:49:21 +0930

    Added websocketpp files

--- Server.cpp
+++ Server.cpp
@@ -1,5 +1,32 @@
 #include "Server.h"
-#include <iostream>
+
+#define MAX_IP_LEN 16
+#define MESSAGE_TYPE_LEN 30
+#define MAX_CLIENTS 100
+#define MAX_KEY_LEN 256
+
+typedef struct {
+    char type[MESSAGE_TYPE_LEN];
+    char sender[MAX_IP_LEN];
+} ServerHelloData;
+
+typedef struct {
+    ServerHelloData data;
+} ServerHelloMessage;
+
+typedef struct {
+    char type[MESSAGE_TYPE_LEN];
+} ClientUpdateRequest;
+
+typedef struct {
+    char type[MESSAGE_TYPE_LEN];
+    char clients[MAX_CLIENTS][MAX_KEY_LEN];
+    int client_count;
+} ClientUpdate;
+
+typedef struct {
+    char type[MESSAGE_TYPE_LEN];
+} ClientListRequest;
 
 Server::Server(const std::string& address, int port) : address(address), port(port), neighbourhood(nullptr) {
     // Initialize server
