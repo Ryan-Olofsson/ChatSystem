2692bd35047b4a917c6a00b8403e5a887ee0c1d4
Kian Esmailzadeh <kesmailzadeh@MacBookAir.gateway>
2024-09-29 22:58:13 +0930

    sup cuties

---
+++ chatapp/_init_.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+fromt .e
+from .routes import main
+
+def create_app():
+    app = Flask(__name__)
+    app.config['DEBUG'] = True
+    app.config['SECRET_KEY'] = 'secret'
+
+    app.register_blueprint(main)
+    return app
+
