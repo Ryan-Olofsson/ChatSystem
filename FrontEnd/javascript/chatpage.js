2f60d2e55e494608ab779de772458f4259cd4dfe
Cray0la <tyler.chappos@gmail.com>
2024-09-29 18:27:35 +0930

    Added selecting which user to talk to

--- FrontEnd/javascript/chatpage.js
+++ FrontEnd/javascript/chatpage.js
@@ -20,9 +20,14 @@ document.querySelector('form').addEventListener('submit', sendMessage);
 
 // Listen for messages
 socket.addEventListener('message', ({ data }) => {
-    const li = document.createElement('li');
-    li.textContent = data;
-    document.querySelector('ul').appendChild(li);
+    const message = JSON.parse(data);
+    if (message.type === 'userList') {
+        updateOnlineUsersList(message.users);
+    } else {
+        const li = document.createElement('li');
+        li.textContent = message.text;
+        document.querySelector('ul#styledList').appendChild(li);
+    }
 });
 
 // Retrive online users
@@ -48,4 +53,21 @@ function retrieveChatHistory() {
 // Upload files to submit
 function uploadFiles() {
 
+}
+
+function selectUser(element) {
+    const selectedUser = element.textContent;
+    document.getElementById('dropdownOnlineUsers').textContent = selectedUser;
+    document.getElementById('selectedChat').textContent = 'Talking to: ' + selectedUser;
+}
+
+// Function to update the user list
+function updateOnlineUsersList(users) {
+    const userList = document.getElementById('userList');
+    userList.innerHTML = ''; // Clear existing list
+    users.forEach(user => {
+        const li = document.createElement('li');
+        li.innerHTML = `<a class="dropdown-item" href="#" onclick="selectUser(this)">${user}</a>`;
+        userList.appendChild(li);
+    });
 }
