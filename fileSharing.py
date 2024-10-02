da7525ff6640c1393d8d232e2f5f8b918c01e4c2
Ryan Olofsson <ryan.olofsson@student.adelaide.edu.au>
2024-10-02 23:06:10 +0930

    Upload file working

--- fileSharing.py
+++ fileSharing.py
@@ -7,14 +7,14 @@ def upload_file(server_url, file_path):
     print(file_path)
     print(server_url)
     # chcks if server_url is valid
-    # if not server_url.startswith('http://') and not server_url.startswith('https://'):
-    #     print("Error: Invalid server URL.")
-    #     return None
+    if not server_url.startswith('http://') and not server_url.startswith('https://'):
+        print("Error: Invalid server URL.")
+        return None
 
-    # # checks if file_path is valid
-    # if not os.path.exists(file_path):
-    #     print("Error: File path is invalid.")
-    #     return None
+    # checks if file_path is valid
+    if not os.path.exists(file_path):
+        print("Error: File path is invalid.")
+        return None
 
     max_file_size = 5 * 1024 * 1024  #  5 MB limit
     file_size = os.path.getsize(file_path)
@@ -22,16 +22,26 @@ def upload_file(server_url, file_path):
     if file_size > max_file_size:
         print("Error: File size too large.")
         return None
-
-    with open(file_path, 'rb') as file:
-        response = requests.post(f"{server_url}/api/upload", files={'file': file})
     
-    if response.status_code == 200:
-        return response.json().get('file_url')
-    elif response.status_code == 413:
-        print("Error: File size too large.")
-    else:
-        print(f"Error: Unable to upload file. Status code: {response.status_code}")
+    file_url = f"{server_url}/{file_path}"
+    print(file_url)
+
+    return {
+        "body": {
+            "file_url": file_url
+        }
+    }
+
+
+    # with open(file_path, 'rb') as file:
+    #     response = requests.post(f"{server_url}/api/upload", files={'file': file})
+    
+    # if response.status_code == 200:
+    #     return response.json().get('file_url')
+    # elif response.status_code == 413:
+    #     print("Error: File size too large.")
+    # else:
+    #     print(f"Error: Unable to upload file. Status code: {response.status_code}")
 
 def retrieve_file(file_url, download_path="downloaded_file"):
     """Retrieves a file from the server."""
